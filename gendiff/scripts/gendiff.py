14b534f619ee47a4e95f133d68a6bea2d76c3bdd
София <sofia@MacBook-Air-Sofia.local>
2026-01-20 19:55:33 +0300

    add .

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,7 +2,7 @@ import sys
 
 def main():
     gendiff_text = '''gendiff -h
-usage: gendiff [-h] first_file second_file
+usage: gendiff [-h] [-f FORMAT] first_file second_file
 
 Compares two configuration files and shows a difference.
 
@@ -10,8 +10,10 @@ positional arguments:
   first_file
   second_file
 
-options:
-  -h, --help            show this help message and exit'''
+options::
+  -h, --help            show this help message and exit
+  -f FORMAT, --format FORMAT
+                        set format of output'''
     if '-h' in sys.argv or '--help' in sys.argv:
       print(gendiff_text)
 
