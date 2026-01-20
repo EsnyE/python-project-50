9c4801748818ad26d49d9946750ec97a4895419c
София <sofia@MacBook-Air-Sofia.local>
2026-01-20 18:32:49 +0300

    add .

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,19 @@
+import sys
+
 def main():
-    print("Welcome to the Gendiff!")
+    gendiff_text = '''gendiff -h
+usage: gendiff [-h] first_file second_file
+
+Compares two configuration files and shows a difference.
+
+positional arguments:
+  first_file
+  second_file
+
+options:
+  -h, --help            show this help message and exit'''
+    if '-h' in sys.argv or '--help' in sys.argv:
+      print(gendiff_text)
 
 
 if __name__ == "__main__":
