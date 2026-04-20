80b29d70d3f8b636b68df6ef30b51a74343101d6
София <sofia@MacBook-Air-Sofia.local>
2026-04-20 21:40:18 +0300

    add .

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,7 +1,7 @@
-from formatters.stylish import format_stylish
-from formatters.plain import format_plain
-from formatters.json import format_json
 
+from gendiff.formatters.stylish import format_stylish
+from gendiff.formatters.plain import format_plain
+from gendiff.formatters.json import format_json
 
 def apply_format(ast: list, format_name: str) -> str:
   
