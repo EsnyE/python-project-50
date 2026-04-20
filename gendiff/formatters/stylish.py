cfc0bfa4e6bdc4d799ef96d1373f21457c7f7c37
София <sofia@MacBook-Air-Sofia.local>
2026-04-20 22:29:35 +0300

    add .

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -1,6 +1,7 @@
-def stylish(diff):
+def format_stylish(diff):
     result = '{\n' + tree_view(diff) + '\n}'
     return result
+
 
 def tree_view(diff, depth=1):
     lines = []
@@ -9,54 +10,50 @@ def tree_view(diff, depth=1):
     result = '\n'.join(lines)
     return result
 
+
 def create_formatted_line(lines, key, value, depth, type):
-    prefix = '  '
     if type == 'nested':
-        indent = (4 * depth - 2) * ' '
+        indent = '    ' * depth
         child_diff = tree_view(value, depth + 1)
-        formated_value = f'{{\n{child_diff}\n{indent}  }}'
-        lines = add_indent_and_format(depth, lines, prefix, key, formated_value)
+        formatted_value = f'{{\n{child_diff}\n{indent}}}'
+        lines.append(f"{indent}    {key}: {formatted_value}")
     elif type == 'changed':
         lines = changed_data_diff(lines, value, depth, key)
     elif type == 'added':
-        prefix = '+ '
-        lines = add_indent_and_format(depth, lines, prefix, key, value)
+        indent = '    ' * (depth - 1)
+        formatted_value = format_value(value, depth + 1)
+        lines.append(f"{indent}  + {key}: {formatted_value}")
     elif type == 'removed':
-        prefix = '- '
-        lines = add_indent_and_format(depth, lines, prefix, key, value)
+        indent = '    ' * (depth - 1)
+        formatted_value = format_value(value, depth + 1)
+        lines.append(f"{indent}  - {key}: {formatted_value}")
     else:
-        lines = add_indent_and_format(depth, lines, prefix, key, value)
+        indent = '    ' * (depth - 1)
+        formatted_value = format_value(value, depth + 1)
+        lines.append(f"{indent}    {key}: {formatted_value}")
     return lines
 
-def add_indent_and_format(depth, lines, prefix, key, value):
-    formated_value = format_value(value, depth + 1)
-    indent = (4 * depth - 2) * ' '
-    lines.append(f"{indent}{prefix}{key}: {formated_value}")
-    return lines
 
 def format_value(value, depth):
     if isinstance(value, dict):
-        prefix = '  '
-        lines = ['{']
+        indent = '    ' * (depth - 1)
+        lines = []
         for key, val in value.items():
-            formated_value = format_value(val, depth + 1)
-            lines = add_indent_and_format(depth, lines,
-                                          prefix, key, formated_value)
-        indent = ' ' * (4 * (depth - 1))
-        lines.append(f"{indent}}}")
-        return '\n'.join(lines)
+            formatted_value = format_value(val, depth + 1)
+            lines.append(f"{'    ' * depth}{key}: {formatted_value}")
+        return '{\n' + '\n'.join(lines) + '\n' + indent + '}'
     elif isinstance(value, bool):
         return str(value).lower()
     elif value is None:
         return 'null'
-    elif isinstance(value, str):
-        return value
     return str(value)
+
 
 def changed_data_diff(lines, value, depth, key):
     old, new = value
-    old_new_pairs = [('- ', old), ('+ ', new)]
-    for prefix, value in old_new_pairs:
-        formated_value = format_value(value, depth + 1)
-        lines = add_indent_and_format(depth, lines, prefix, key, formated_value)
+    indent = '    ' * (depth - 1)
+    formatted_old = format_value(old, depth + 1)
+    formatted_new = format_value(new, depth + 1)
+    lines.append(f"{indent}  - {key}: {formatted_old}")
+    lines.append(f"{indent}  + {key}: {formatted_new}")
     return lines
